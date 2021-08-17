57522891bf5318edaadd1390ce64d355c501b723
peytonratliff <ratliffbox@gmail.com>
2021-08-17 18:20:16 -0400

    starting XML loading

--- js/classSwap.js
+++ js/classSwap.js
@@ -7,47 +7,47 @@ function classSwap(classID) {
     document.getElementById("startString").innerHTML = "";
 
 
-    if(classID == "druidIcon"){
+    if(classID == "druid"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-druid-balance.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-druid-feral-combat.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-druid-restoration.jpg')";
     }
-    else if(classID == "hunterIcon"){
+    else if(classID == "hunter"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-hunter-beast-mastery.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-hunter-marksmanship.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-hunter-survival.jpg')";
     }
-    else if(classID == "mageIcon"){
+    else if(classID == "mage"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-mage-arcane.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-mage-fire.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-mage-frost.jpg')";
     }
-    else if(classID == "paladinIcon"){
+    else if(classID == "paladin"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-paladin-holy.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-paladin-protection.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-paladin-retribution.jpg')";
     }
-    else if(classID == "priestIcon"){
+    else if(classID == "priest"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-priest-discipline.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-priest-holy.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-priest-shadow.jpg')";
     }
-    else if(classID == "rogueIcon"){
+    else if(classID == "rogue"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-rogue-assassination.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-rogue-combat.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-rogue-subtlety.jpg')";
     }
-    else if(classID == "shamanIcon"){
+    else if(classID == "shaman"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-shaman-elemental.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-shaman-enhancement.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-shaman-restoration.jpg')";
     }
-    else if(classID == "warlockIcon"){
+    else if(classID == "warlock"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-warlock-affliction.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-warlock-demonology.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-warlock-destruction.jpg')";
     }
-    else if(classID == "warriorIcon"){
+    else if(classID == "warrior"){
         column1.style.backgroundImage = "url('../images/spec backgrounds/background-warrior-arms.jpg')";
         column2.style.backgroundImage = "url('../images/spec backgrounds/background-warrior-fury.jpg')";
         column3.style.backgroundImage = "url('../images/spec backgrounds/background-warrior-protection.jpg')";
@@ -55,4 +55,22 @@ function classSwap(classID) {
     else{
         alert("Something went wrong");
     }
+
+    fetch('https://peytonratliff.github.io/classicWoWxml/' + classID + ".xml")
+    .then(function(resp){
+        return resp.text();
+    })
+    .then(function(data){
+        let parser = new DOMParser(),
+            xmlDoc = parser.parseFromString(data, 'text/xml');
+        build(xmlDoc, classID);
+    });
+}
+
+function build(xmlDoc, classID){
+    console.log(xmlDoc.getElementsByTagName(classID));
+
+    var cellEdit = document.getElementById("row1col1");
+
+    // cellEdit = xmlDoc.get
 }
