f9fe2647fefa100bfac5def79bf6bd8ff325bbfb
peytonratliff <ratliffbox@gmail.com>
2021-08-23 23:26:53 -0400

    druid balance finished
    finally finished a single spec

--- js/classSwap.js
+++ js/classSwap.js
@@ -94,7 +94,28 @@ function buildDruid(){
     var spec1row2col1 = document.getElementById("spec1row2col1icon");
     var spec1row2col2 = document.getElementById("spec1row2col2icon");
     var spec1row2col3 = document.getElementById("spec1row2col3icon");
-    var spec1row2col4 = document.getElementById("spec1row1col4icon");
+    var spec1row2col4 = document.getElementById("spec1row2col4icon");
+    var spec1row3col1 = document.getElementById("spec1row3col1icon");
+    var spec1row3col2 = document.getElementById("spec1row3col2icon");
+    var spec1row3col3 = document.getElementById("spec1row3col3icon");
+    var spec1row3col4 = document.getElementById("spec1row3col4icon");
+    var spec1row4col1 = document.getElementById("spec1row4col1icon");
+    var spec1row4col2 = document.getElementById("spec1row4col2icon");
+    var spec1row4col3 = document.getElementById("spec1row4col3icon");
+    var spec1row4col4 = document.getElementById("spec1row4col4icon");
+    var spec1row5col1 = document.getElementById("spec1row5col1icon");
+    var spec1row5col2 = document.getElementById("spec1row5col2icon");
+    var spec1row5col3 = document.getElementById("spec1row5col3icon");
+    var spec1row5col4 = document.getElementById("spec1row5col4icon");
+    var spec1row6col1 = document.getElementById("spec1row6col1icon");
+    var spec1row6col2 = document.getElementById("spec1row6col2icon");
+    var spec1row6col3 = document.getElementById("spec1row6col3icon");
+    var spec1row6col4 = document.getElementById("spec1row6col4icon");
+    var spec1row7col1 = document.getElementById("spec1row7col1icon");
+    var spec1row7col2 = document.getElementById("spec1row7col2icon");
+    var spec1row7col3 = document.getElementById("spec1row7col3icon");
+    var spec1row7col4 = document.getElementById("spec1row7col4icon");
+
 
     //Build specBar
     var spec1icon = document.getElementById("spec1icon");
@@ -150,7 +171,6 @@ function buildDruid(){
         spec1row1col2arrow.style.left = "43px"
         spec1row1col2arrow.style.zIndex += 1;
         arrowArray.push(spec1row1col2div, spec1row1col2arrow);
-        console.log(arrowArray);
 
     //Build spec1row1col3 Improved Nature's Grasp
     spec1row1col3.style.display = "block";
@@ -178,6 +198,20 @@ function buildDruid(){
     spec1row2col2.style.backgroundSize = "34px 34px";
     spec1row2col2.style.backgroundPosition = "center center";
     spec1row2col2.style.filter = "grayscale(100%)";
+        //Build arrow
+        var spec1row2col2div = document.createElement('div');
+        spec1row2col2div.style.maxHeight = "0px";
+        spec1row2col2div.style.position = "absolute";
+        spec1row2col2div.id = "spec1row2col2div";
+        document.getElementById('spec1row2col2').appendChild(spec1row2col2div);
+        var spec1row2col2arrow = document.createElement('img');
+        spec1row2col2arrow.src = '../images/table elements/arrow_skip1_down.png';
+        document.getElementById('spec1row2col2div').appendChild(spec1row2col2arrow);
+        spec1row2col2arrow.style.position = "relative";
+        spec1row2col2arrow.style.top = "-3px"
+        spec1row2col2arrow.style.left = "19px"
+        spec1row2col2arrow.style.zIndex += 1;
+        arrowArray.push(spec1row2col2div, spec1row2col2arrow);
 
     //Build spec1row2col3 Natural Weapons
     spec1row2col3.style.display = "block";
@@ -186,14 +220,142 @@ function buildDruid(){
     spec1row2col3.style.backgroundSize = "34px 34px";
     spec1row2col3.style.backgroundPosition = "center center";
     spec1row2col3.style.filter = "grayscale(100%)";
+        //Build arrow
+        var spec1row2col3div = document.createElement('div');
+        spec1row2col3div.style.maxHeight = "0px";
+        spec1row2col3div.style.position = "absolute";
+        spec1row2col3div.id = "spec1row2col3div";
+        document.getElementById('spec1row2col3').appendChild(spec1row2col3div);
+        var spec1row2col3arrow = document.createElement('img');
+        spec1row2col3arrow.src = '../images/table elements/arrow_direct_down.png';
+        document.getElementById('spec1row2col3div').appendChild(spec1row2col3arrow);
+        spec1row2col3arrow.style.position = "relative";
+        spec1row2col3arrow.style.top = "-3px"
+        spec1row2col3arrow.style.left = "19px"
+        spec1row2col3arrow.style.zIndex += 1;
+        arrowArray.push(spec1row2col3div, spec1row2col3arrow);
+    
 
     //Build spec1row2col4 Natural Shapeshifter
-    spec1row2col3.style.display = "block";
-    spec1row2col3.style.backgroundImage = "url('../images/spell icons/druid/talents/natural_shapeshifter.jpg')";
-    spec1row2col3.style.backgroundRepeat = "no-repeat";
-    spec1row2col3.style.backgroundSize = "34px 34px";
-    spec1row2col3.style.backgroundPosition = "center center";
-    spec1row2col3.style.filter = "grayscale(100%)";
+    spec1row2col4.style.display = "block";
+    spec1row2col4.style.backgroundImage = "url('../images/spell icons/druid/talents/natural_shapeshifter.jpg')";
+    spec1row2col4.style.backgroundRepeat = "no-repeat";
+    spec1row2col4.style.backgroundSize = "34px 34px";
+    spec1row2col4.style.backgroundPosition = "center center";
+    spec1row2col4.style.filter = "grayscale(100%)";
+
+    //Build spec1row3col1 Improved Thorns
+    spec1row3col1.style.display = "block";
+    spec1row3col1.style.backgroundImage = "url('../images/spell icons/druid/talents/improved_thorns.jpg')";
+    spec1row3col1.style.backgroundRepeat = "no-repeat";
+    spec1row3col1.style.backgroundSize = "34px 34px";
+    spec1row3col1.style.backgroundPosition = "center center";
+    spec1row3col1.style.filter = "grayscale(100%)";
+
+    //Build spec1row3col2 EMPTY
+    spec1row3col2.style.display = "none";
+
+    //Build spec1row3col3 Omen of Clarity
+    spec1row3col3.style.display = "block";
+    spec1row3col3.style.backgroundImage = "url('../images/spell icons/druid/talents/omen_of_clarity.jpg')";
+    spec1row3col3.style.backgroundRepeat = "no-repeat";
+    spec1row3col3.style.backgroundSize = "34px 34px";
+    spec1row3col3.style.backgroundPosition = "center center";
+    spec1row3col3.style.filter = "grayscale(100%)";
+
+    //Build spec1row3col4 Nature's Reach
+    spec1row3col4.style.display = "block";
+    spec1row3col4.style.backgroundImage = "url('../images/spell icons/druid/talents/natures_reach.jpg')";
+    spec1row3col4.style.backgroundRepeat = "no-repeat";
+    spec1row3col4.style.backgroundSize = "34px 34px";
+    spec1row3col4.style.backgroundPosition = "center center";
+    spec1row3col4.style.filter = "grayscale(100%)";
+
+    //Build spec1row4col1 EMPTY
+    spec1row4col1.style.display = "none";
+
+    //Build spec1row4col2 Vengeance
+    spec1row4col2.style.display = "block";
+    spec1row4col2.style.backgroundImage = "url('../images/spell icons/druid/talents/vengeance.jpg')";
+    spec1row4col2.style.backgroundRepeat = "no-repeat";
+    spec1row4col2.style.backgroundSize = "34px 34px";
+    spec1row4col2.style.backgroundPosition = "center center";
+    spec1row4col2.style.filter = "grayscale(100%)";
+
+    //Build spec1row4col3 Improved Starfire
+    spec1row4col3.style.display = "block";
+    spec1row4col3.style.backgroundImage = "url('../images/spell icons/druid/talents/improved_starfire.jpg')";
+    spec1row4col3.style.backgroundRepeat = "no-repeat";
+    spec1row4col3.style.backgroundSize = "34px 34px";
+    spec1row4col3.style.backgroundPosition = "center center";
+    spec1row4col3.style.filter = "grayscale(100%)";
+
+    //Build spec1row4col4 EMPTY
+    spec1row4col4.style.display = "none";
+
+    //Build spec1row5col1 EMPTY
+    spec1row5col1.style.display = "none";
+
+    //Build spec1row5col2 Nature's Grace
+    spec1row5col2.style.display = "block";
+    spec1row5col2.style.backgroundImage = "url('../images/spell icons/druid/talents/natures_grace.jpg')";
+    spec1row5col2.style.backgroundRepeat = "no-repeat";
+    spec1row5col2.style.backgroundSize = "34px 34px";
+    spec1row5col2.style.backgroundPosition = "center center";
+    spec1row5col2.style.filter = "grayscale(100%)";
+        //Build arrow
+        var spec1row5col2div = document.createElement('div');
+        spec1row5col2div.style.maxHeight = "0px";
+        spec1row5col2div.style.position = "absolute";
+        spec1row5col2div.id = "spec1row5col2div";
+        document.getElementById('spec1row5col2').appendChild(spec1row5col2div);
+        var spec1row5col2arrow = document.createElement('img');
+        spec1row5col2arrow.src = '../images/table elements/arrow_direct_down.png';
+        document.getElementById('spec1row5col2div').appendChild(spec1row5col2arrow);
+        spec1row5col2arrow.style.position = "relative";
+        spec1row5col2arrow.style.top = "-3px"
+        spec1row5col2arrow.style.left = "19px"
+        spec1row5col2arrow.style.zIndex += 1;
+        arrowArray.push(spec1row5col2div, spec1row5col2arrow);
+
+    //Build spec1row5col3 Moonglow
+    spec1row5col3.style.display = "block";
+    spec1row5col3.style.backgroundImage = "url('../images/spell icons/druid/talents/moonglow.jpg')";
+    spec1row5col3.style.backgroundRepeat = "no-repeat";
+    spec1row5col3.style.backgroundSize = "34px 34px";
+    spec1row5col3.style.backgroundPosition = "center center";
+    spec1row5col3.style.filter = "grayscale(100%)";
+
+    //Build spec1row5col4 EMPTY
+    spec1row5col4.style.display = "none";
+
+    //Build spec1row6col1 EMPTY
+    spec1row6col1.style.display = "none";
+
+    //Build spec1row6col2 Moonfury
+    spec1row6col2.style.display = "block";
+    spec1row6col2.style.backgroundImage = "url('../images/spell icons/druid/talents/moonfury.jpg')";
+    spec1row6col2.style.backgroundRepeat = "no-repeat";
+    spec1row6col2.style.backgroundSize = "34px 34px";
+    spec1row6col2.style.backgroundPosition = "center center";
+    spec1row6col2.style.filter = "grayscale(100%)";
+
+    //Build spec1row6col3 EMPTY
+    spec1row6col3.style.display = "none";
+
+    //Build spec1row6col4 EMPTY
+    spec1row6col4.style.display = "none";
+
+    //Build spec1row7col1 EMPTY
+    spec1row7col1.style.display = "none";
+
+    //Build spec1row7col2 Moonkin Form
+    spec1row7col2.style.display = "block";
+    spec1row7col2.style.backgroundImage = "url('../images/spell icons/druid/talents/moonkin_form.jpg')";
+    spec1row7col2.style.backgroundRepeat = "no-repeat";
+    spec1row7col2.style.backgroundSize = "34px 34px";
+    spec1row7col2.style.backgroundPosition = "center center";
+    spec1row7col2.style.filter = "grayscale(100%)";
 }
 
 function buildHunter(){
